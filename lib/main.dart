aaba895c2bf2ec6d21d24d9093fdab8289e5ce7c
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2024-10-11 19:43:33 -0400

    ok looks good

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(healthappbgc());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class healthappbgc extends StatelessWidget {
+  const healthappbgc({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 255, 255)),
         ),
         home: MyHomePage(),
       ),
@@ -27,20 +27,65 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+    void getNext() {
+      current = WordPair.random();
+      notifyListeners();
+    }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair =appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RandomText(pair: pair),
+            SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: (){
+                appState.getNext();
+              },
+              child:Text("press me")
+              ),     
+          ],
+        ),
       ),
     );
   }
+}
+
+class RandomText extends StatelessWidget {
+  const RandomText({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  // ...
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,    // ← And also this.
+      child: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Text(pair.asLowerCase, style: style,),
+      ),
+    );
+  }
+
+// ...
 }
