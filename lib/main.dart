416454aa8a4055fa468a060ff0858b53fe28a129
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2024-10-15 20:35:23 -0400

    more of the totourial

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -19,7 +20,7 @@ class healthappbgc extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 255, 255)),
         ),
-        home: MyHomePage(),
+        home: homePage(),
       ),
     );
   }
@@ -32,33 +33,123 @@ class MyAppState extends ChangeNotifier {
       current = WordPair.random();
       notifyListeners();
     }
+
+    var favorites = <WordPair>[];
+
+    void toggleFavorite() {
+      if (favorites.contains(current)){
+        favorites.remove(current);
+      }else {
+        favorites.add(current);
+      }
+      notifyListeners();
+    } 
 }
 
-class MyHomePage extends StatelessWidget {
+// ...
+
+class homePage extends StatefulWidget {
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair =appState.current;
-
+    Widget page;
+switch (selectedIndex) {
+  case 0:
+    page = GeneratorPage();
+    break;
+  case 1:
+    page = Placeholder();
+    break;
+  default:
+    throw UnimplementedError('no widget for $selectedIndex');
+}
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RandomText(pair: pair),
-            SizedBox(height: 15),
-            ElevatedButton(
-              onPressed: (){
-                appState.getNext();
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });
               },
-              child:Text("press me")
-              ),     
-          ],
-        ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RandomText(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// ...
 
 class RandomText extends StatelessWidget {
   const RandomText({
