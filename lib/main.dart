cea0f382bfc815267e0ab0f8909c78d4a5e2e9e0
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2024-10-18 12:13:32 -0400

    organization changes
    seperated pages and related widgets into multiple dart files, also imported sqflite for future database implementation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'home.dart';
+import 'exportMenu.dart';
 
 void main() {
   runApp(healthappbgc());
@@ -61,10 +63,8 @@ class _homePageState extends State<homePage> {
 switch (selectedIndex) {
   case 0:
     page = GeneratorPage();
-    break;
   case 1:
     page = Placeholder();
-    break;
   default:
     throw UnimplementedError('no widget for $selectedIndex');
 }
@@ -102,81 +102,4 @@ switch (selectedIndex) {
       ),
     );
   }
-}
-
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          RandomText(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-// ...
-
-class RandomText extends StatelessWidget {
-  const RandomText({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  // ...
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,    // ← And also this.
-      child: Padding(
-        padding: const EdgeInsets.all(40),
-        child: Text(pair.asLowerCase, style: style,),
-      ),
-    );
-  }
-
-// ...
 }
