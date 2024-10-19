f4f749a0807fbde7a38deaf86b4f0d7f2ce09b62
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2024-10-19 10:35:50 -0400

    More studentMenu functionality
    Added modal for add student button with textFormField

--- lib/studentMenu.dart
+++ lib/studentMenu.dart
@@ -22,6 +22,7 @@ class GeneratorPage extends StatelessWidget {
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton.icon(
                     onPressed: () {
+                      dialogBuilder(context);
                     },
                     label: Text('Add Student'),
                   ),
@@ -31,6 +32,42 @@ class GeneratorPage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+  Future<void> dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Please input the students ID'),
+          content: TextFormField(
+            decoration: const InputDecoration(
+    border: UnderlineInputBorder(),
+    labelText: 'student ID',
+          ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Submit'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
